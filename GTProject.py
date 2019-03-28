3db6d058688aa7d58317229f8a34c2150f696cc6
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2019-03-28 18:03:38 +0000

    Start of Shunting yard Algorithm

--- GTProject.py
+++ GTProject.py
@@ -5,3 +5,48 @@
 
 
 
+# Argument containing a infix regular exspression
+def shunt(infix):
+
+    # Precedence given to the regular exspression using a python dictionary
+    specials = {'*': 50, '.': 40, '|': 30 }
+
+    # Postfix regular exspression and stack is where the operators will be pushed and poped. 
+    pofix = ""
+    stack = ""
+
+
+    # Shunting Yard Algorithm
+    for c in infix:
+        if c == '(':
+            stack = stack + c
+        
+        # Takes what is on the end of the stack and place it into pofix and then 
+        # remove it from the stack and finally it removes the '(' from the stack
+        elif c == ')':
+            while stack[-1] != '(': 
+                pofix, stack = pofix + stack[-1], stack[:-1]
+            stack = stack[:-1]
+
+        # Takes what is on the end of the stack and place it into pofix and then remove
+        #  it from the stack and then places the special character onto the stack
+        elif c in specials:
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
+                pofix, stack = pofix + stack[-1], stack[:-1]
+            stack = stack + c
+            
+        else:
+            pofix = pofix + c
+
+    # Takes what is on the end of the stack and place it into pofix and then remove it from the stack
+    while stack:
+        pofix, stack = pofix + stack[-1], stack[:-1]
+        
+
+
+
+
+    return pofix
+
+
+
