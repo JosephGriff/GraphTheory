4a3a2760e112eabe7610c9c4f2797ba8c23ccf7d
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2019-03-29 20:25:43 +0000

    Creation of Python classes

--- thompsons.py
+++ thompsons.py
@@ -1,3 +1,19 @@
 # Thompson's construction
 # Joseph Griffith
 # G00350112
+
+# Represents a state with two arrows, labelled by label.
+# Use None for a label representing "e" arrows.
+class state:
+    label = None
+    edge1 = None
+    edge2 = None
+
+# an NFA is represented by its initial and accept states.
+class nfa:
+    initial = None
+    accept = None
+
+    def __init__(self, initial, accept):
+        self.initial = initial
+        self.accept = accept
